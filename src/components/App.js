158420c857aa5cf7d698347b67f236539c4dbf67
Kumar Abhirup <kumar@iqubex.com>
2019-01-09 23:00:52 +0530

    Hooray!! 🚀

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../assets/images/kumar.jpeg';
+import '../assets/styles/App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" style={{borderRadius: '100%'}} />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            kumarabhirup.com gets a headstart!
           </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
+            href="https://kumarabhirup.com"
             rel="noopener noreferrer"
           >
-            Learn React
+            Kumar Abhirup
           </a>
         </header>
       </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from 'react'; import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+
+import './assets/styles/index.css';
+import App from './components/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
